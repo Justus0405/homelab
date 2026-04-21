71d858b136ad7be7bc2632eb27ae7d2ea00728e4
Justus0405 <justus040506@gmail.com>
2026-04-21 13:57:09 +0200

    Updated Debian Scripts

--- scripts/Debian-install-docker.sh
+++ scripts/Debian-install-docker.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d "/etc/apt/keyrings"
-sudo curl -fsSL "https://download.docker.com/linux/debian/gpg" -o "/etc/apt/keyrings/docker.asc"
-sudo chmod a+r "/etc/apt/keyrings/docker.asc"
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" |
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Setup Docker's permissions
 sudo groupadd -f docker
--- scripts/Debian-install-playit.sh
+++ scripts/Debian-install-playit.sh
@@ -2,12 +2,11 @@
 
 sudo apt install gpg curl -y
 
-curl -SsL "https://playit-cloud.github.io/ppa/key.gpg" | gpg --dearmor | sudo tee "/etc/apt/trusted.gpg.d/playit.gpg" >/dev/null
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
-sudo apt install playit
+sudo apt install playit -y
 
-sudo systemctl enable playit
-sudo systemctl start playit
+sudo systemctl enable --now playit
 
 playit setup
